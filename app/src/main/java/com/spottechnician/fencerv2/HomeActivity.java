1b0305ede2b0f4492418014f9530fa542ff7725f
MilindAmrutkar <milind27.amrutkar@gmail.com>
2017-03-15 09:10:25 +0530

    Initial commit

---
+++ app/src/main/java/com/spottechnician/fencerv2/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.spottechnician.fencerv2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void Services(View view) {
+        Intent intent = new Intent(HomeActivity.this, ServicesActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/spottechnician/fencerv2/ServicesActivity.java
@@ -0,0 +1,18 @@
+package com.spottechnician.fencerv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ServicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services);
+    }
+
+    public void HOTOActivity(View view) {
+        
+    }
+}
